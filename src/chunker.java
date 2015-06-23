ce6f755c5f211153e48e93058e95244d4a175119
kenoboss <tziegelmayer@web.de>
2015-06-23 17:22:40 +0200

    first rules

---
+++ src/chunker.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
+public class chunker {
+
+	public static void main(String[] args) {
+		
+		final long timeStart = System.currentTimeMillis();
+		
+		String [] corpus_chunked = new String [1574818];
+		String [] corpus_tagged = new String [1574818];
+
+		try {
+			// Reading the diffrent corpora
+			// Creating an array for the chunked corpus
+			File chunked_corpus = new File("C:/Users/Kenobi/workspace/Chunker_POS_Tagger_Test/newCorporus/chunked_corpus.txt");
+			BufferedReader incc  = new BufferedReader(new InputStreamReader(new FileInputStream (chunked_corpus), "UTF8"));			
+			String line = null;
+
+			int indexcc = 0;
+			while (( line = incc.readLine()) != null) {
+				corpus_chunked[indexcc] = line;
+				indexcc++;
+			}
+			incc.close();
+
+			// Creating an array for the tagged corpus (POS-Tag)
+			
+			File tagged_corpus = new File("C:/Users/Kenobi/workspace/Chunker_POS_Tagger_Test/newCorporus/tagged_corpus.txt");
+			BufferedReader intc  = new BufferedReader(new InputStreamReader(new FileInputStream (tagged_corpus), "UTF8"));
+			
+			int indextc = 0;
+			while (( line = intc.readLine()) != null) {
+				corpus_tagged[indextc] = line;
+				indextc++;
+			}
+			intc.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();  
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+			String [] ctags = {"B-NC", "I-NC", "B-VC", "I-VC", "B-PC", "I-PC"};
+			String [] postag = {"ADJA", "ADV", "APPR", "ART", "CARD", "NN", "NE", "PPER", "VVFIN", "VVIMP"};
+			String [] rules = new String [ctags.length*postag.length];
+
+			// Creating the first rules
+			for (int j = 0; j < postag.length; j++){
+				for (int k = 0; k < ctags.length; k++){
+					for (int l = 0; l < rules.length; l++){
+						rules[l] = "[POS, 0="+postag[j]+"] = "+ctags[k];
+					}
+				}
+			}
+			
+			for (int i= 0; i<corpus_tagged.length; i++){
+				Token tok = new Token (corpus_tagged[i]);
+				System.out.println(tok.getTag());
+			}
+			
+
+		final long timeEnd = System.currentTimeMillis(); 
+		final long time = (timeEnd - timeStart)/1000;
+		System.out.println("Dauer des Programms: " + time + " Sek."); 
+
+	}
+
+}
